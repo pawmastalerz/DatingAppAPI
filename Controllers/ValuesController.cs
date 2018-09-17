c4edff182733f32b8e7746a0fcd03aa728c2521d
Pawel <paw.mastalerz@gmail.com>
2018-09-17 15:57:21 +0200

    Login and registration

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingAppAPI.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
