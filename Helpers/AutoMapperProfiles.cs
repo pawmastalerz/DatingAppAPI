6dbc4789fec65f504ca4f5da8811bfa984317852
Pawel <paw.mastalerz@gmail.com>
2018-09-20 14:35:37 +0200

    Photo upload to cloudinary.

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -29,6 +29,8 @@ namespace DatingAppAPI.Helpers
             });
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
