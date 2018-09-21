325edd89080b479f2ce6bdc66fc1b3351f48a641
Pawel <paw.mastalerz@gmail.com>
2018-09-21 15:23:12 +0200

    Reactive registration form & photo upload.

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -31,6 +31,7 @@ namespace DatingAppAPI.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
